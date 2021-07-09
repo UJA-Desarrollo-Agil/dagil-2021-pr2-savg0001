b072fe5ac2d622e164ad1788ac3e124aa8f8b84c
Sebastian Alberto Vico Guzman <savg0001@red.ujaen.es>
2021-07-09 19:53:29 +0200

    Situacion final 2

--- games/media/games/codigo.js
+++ games/media/games/codigo.js
@@ -122,16 +122,23 @@ undum.game.situations = {
 
   /*Elijo volver a casa sin ir al mercadona*/
   ircasa: new undum.SimpleSituation(
-      "<p>Vuelvo a casa minetras desayuno tranquilamente, y me doy cuenta de que todos los escaparates de las tiendas del barrio están cerrados, lo cual me resulta muy extraño</p>\
+      "<p>Vuelvo a casa mientras desayuno tranquilamente, y me doy cuenta de que todos los escaparates de las tiendas del barrio están cerrados, lo cual me resulta muy extraño</p>\
       <p>Saco mi móvil del bolsillo para buscar en internet que pasa cuando me da por mirar la fecha...¡Es domingo! Menos mal que no decidí ir al Mercadona porque estaría cerrado</p>\
-      <p>Así que prosigo mi camino aliviado por no haber hecho unos metros inútiles hasta que llego a casa.</p>"
+      <p>Así que prosigo mi camino aliviado por no haber hecho unos metros inútiles hasta que llego a casa.</p>",
 
   ),
 
   /*Elijo ir al mercadona, comprar y volver a casa*/
-  irmercadonamfin: new undum.SimpleSituation(
-
+  irmercadonafin: new undum.SimpleSituation(
+    "<p>Cuando llego al Mercadona me quedo asombrado, ¡Está cerrado! Que extraño me parece, decido sacar mi móvil para buscar en internet que ha pasado cuando me da por mirar la fecha.</p>\
+    <p>¡Hoy es domingo! Que torpe soy, será también cosa de las horas de estudio...Pues nada, toca volver a casa pero no me rendiré en mi lucha por desayunar algo rico. Ahora sí, toca <a href='churrosfin'>comprar esos churros</a>.</p>",
   ),
+
+  churrosfin: new undum.SimpleSituation(
+      "<p>LLego a la churrería rápidamente porque mi estómago no para de rugir y por fin obtengo mi preciado desayuno, ya tendré tiempo de empezar la operación bikini mañana.</p>\
+      <p>Así que prosigo mi camino aliviado por haber comido algo hasta que llego a casa.</p>",
+  )
+
 
 
 
